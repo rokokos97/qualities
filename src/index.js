370bafb128ce0955acbe578d1f87a6b9c8631818
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-23 13:06:17 +0100

    import import * as Sentry from "@sentry/react"; import { BrowserTracing } from "@sentry/tracing";
    to index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
+Sentry.init({
+    dsn: "https://6fd7cbae86e34dbf93e907823f5960e0@o4504553700261888.ingest.sentry.io/4504553739911168",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
