a799a5696b751e8227885604ccc6445a5b3b9806
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-24 19:23:56 +0100

    extractions methods get/put from editQuality to *service
    show error fot client

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,39 +1,46 @@
 import React, { useEffect, useState } from "react";
 import EditForm from "../components/ui/editForm";
 import { useParams } from "react-router-dom";
-import httpService from "../services/httpService";
-import config from "../config.json"
+import qualityService from "../services/quality.Service";
+import {toast} from "react-toastify";
+
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null);
     const id = useParams().id
-    const qualityEndPoint = config.apiEndPoint + `quality/${id}`
-
-
+    const [, setErrors] = useState(null);
     const updateQuality = async (content) => {
         try {
-            const { data } = await httpService.put(qualityEndPoint, content);
-            return data;
-        } catch (error) {
-            console.log("ExpectedError");
+            const data = await qualityService.update(id,content);
+            return data.content
+        } catch (error){
+            const { message, code } = error.response.data
+            toast.error(message)
+            setErrors({ message, code })
         }
     };
     const getQuality = async (id) => {
         try {
-            const { data } = await httpService.get(qualityEndPoint, id);
-            return data;
+            const  data  = await qualityService.get(id);
+            console.log(data);
+            return data.content;
         } catch (error) {
             console.log("ExpectedError");
         }
     }
-
-    const handleSubmit = (data) => { updateQuality(data) }
+    const handleSubmit = (data) => {
+        console.log(data);
+        updateQuality(data)
+    };
     useEffect(() => {
-        getQuality(id).then(data=>setQuality(data.content))
-    },[]);
+        getQuality(id).then((data) => setQuality(data)
+        );
+    }, []);
     return (
         <>
              <h1>Edit Quality Page</h1>
-            {quality!==null?<EditForm data={quality} onSubmit={handleSubmit}/>:"Loading"}
+            {quality !==null
+                ?<EditForm data={quality} onSubmit={handleSubmit}/>
+                :"Loading"}
         </>
     );
 };
---
+++ src/app/services/quality.Service.js
@@ -0,0 +1,16 @@
+import httpService from "./httpService";
+const qualityEndpoint = "quality/";
+
+const qualityService = {
+    update: async (id, content) => {
+        const { data } = await httpService.put(
+            qualityEndpoint+id+"dfsa",
+            content)
+        return data
+    },
+    get: async (id) => {
+        const { data } = await httpService.get(qualityEndpoint+id)
+        return data;
+    }
+}
+export default qualityService;
