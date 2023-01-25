cc61d11144d1ca73dfa053eac6eef67f827a76a5
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-25 16:52:14 +0100

    create new state for error in QualitiesProvider

--- src/app/hooks/useQualities.jsx
+++ src/app/hooks/useQualities.jsx
@@ -1,22 +1,27 @@
 import React, {useContext, useEffect, useState} from "react";
 import qualityService from "../services/quality.Service";
+import {toast} from "react-toastify";
 
 const QualitiesContext = React.createContext();
 export const  useQualities = () => {
     return useContext(QualitiesContext);
 };
 export const QualitiesProvider = ({children}) => {
-    const [qualities, setQualities] = useState();
+    const [qualities, setQualities] = useState([]);
+    const [error, setError] = useState(null);
     useEffect(()=>{
         const getQualities = async () => {
             try {
                 const qualities = await qualityService.fetchAll()
                 setQualities(qualities)
             } catch (error){
-
+                const { message } = error.response.data
+                toast.error(message);
+                setError(message)
             }
         }
-    })
+        getQualities();
+    }, []);
     return <QualitiesContext.Provider value={qualities}>
         {children}
     </QualitiesContext.Provider>
